14b20a32ed6a23feab23d4cf2b5fd197a3743511
Bharathi-2109 <120362711+Bharathi-2109@users.noreply.github.com>
2022-12-19 09:48:33 +0530

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//Homepage Component
+function Home() {
+  return (
+    <div className="app">
+      <h1>Welcome to our Home Page </h1>
+    </div>
+  );
+}
+
+export default Home;
